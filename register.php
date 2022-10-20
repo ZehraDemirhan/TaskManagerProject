c9758cf2b7989a21a7af0d31a611d15ca63f90e7
ZehraDemirhan <105852182+ZehraDemirhan@users.noreply.github.com>
2022-10-20 13:13:55 +0300

    update ajax

--- register.php
+++ register.php
@@ -10,15 +10,17 @@
     <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js"></script>
    
     <link rel="stylesheet" href="font-awesome-4.7.0/css/font-awesome.min.css">
-  
-    
-   <style>
+
+    <link rel="stylesheet" href="style.css">
+    <style>
     .center {
   margin: auto;
   width: 50%;
  
   padding: 10px;
 }
+
+
    </style>
   </head>
 <body>
@@ -30,12 +32,68 @@
  //extract($_FILES);
   if(!empty($_POST))
   {
-    $file= $_FILES['file']['tmp_name'];
+    $file= $_FILES['file'];
+
+    $fileName = $_FILES['file']['name'];
+    $fileTmpName = $_FILES['file']['name'];
+    $fileSize = $_FILES['file']['name'];
+    $fileError = $_FILES['file']['name'];
+    $fileType= $_FILES['file']['name'];
+
+    $fileExt = explode('.',$fileName);
+    $fileActualExt = strtolower(end($fileExt));
+
+    $allowed = array('jpg', 'jpeg', 'png');
+
+    if (in_array($fileActualExt, $allowed ))
+    {
+        if($file === 0)
+        {
+            if($fileSize < 1000000)
+            {
+                //Create random unique id to distinguish the files
+                $fileNameNew = uniqid('', true ).".".$fileActualExt;
+
+                $fileDestination = 'images/'.$fileNameNew;
+
+                move_uploaded_file($fileTmpName, $fileDestination);
+
+
+
+
+
+            }
+            else{
+                $fileTypeError = 1;
+            }
+
+
+        }
+        else{
+            $fileTypeError = 1;
+        }
+
+
+    }
+
+    else
+    {
+        $fileTypeError = 1;
+
+    }
+
+
+
+
+
+
 
     $_SESSION['user']['image']=$file;
     var_dump($_SESSION);
     extract($_POST);
     registerUser($name,$email,$pass,$file);
+
+
     header("Location: index.php");
   }
 
@@ -53,7 +111,7 @@
   </nav>
   
   <div class="center">
-  <div class="row" >
+  <div class="row" >//enctype for passing images
     <form class="" method="post"  enctype="multipart/form-data" >
       <div class="row" >
         <div class="input-field " >
@@ -95,6 +153,12 @@
     <div class="" style="text-align:center;"><button class="btn waves-effect waves-light" type="submit" name="action">Register
     <i class="material-icons right">send</i>
   </button></div> </div>
+
+        <div class="row" style="text-align:center;">
+            <div class=<?= isset($fileTypeError) && $fileTypeError==1 ? "elementToFadeInAndOut":"hide" ?>>
+                Wrong file type!
+            </div>
+        </div>
     
 
   </div>
